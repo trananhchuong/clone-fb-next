ad680a4f3d2079d4df81862262723a9073ba3da3
Chuong Tran <39957959+trananhchuong@users.noreply.github.com>
2021-07-11 11:48:24 +0700

    add config domain img

--- fbclone-next/pages/index.js
+++ fbclone-next/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -11,15 +12,16 @@ export default function Home() {
       </Head>
 
       <h1>Let build Facebook</h1>
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div className="flex-shrink-0">
-          <img className="h-12 w-12" src="https://docosan.com/static/Medical-certi-final.png" alt="ChitChat Logo" />
-        </div>
-        <div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-gray-500">You have a new message!</p>
-        </div>
-      </div>
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+
+        {/* Feed */}
+        
+        {/* Widgets */}
+      </main>
+
     </div>
   )
 }
